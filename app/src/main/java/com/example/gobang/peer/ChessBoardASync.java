67c2f1c2fd28441872ba190af4d5875fa8367354
paul <paul2309825@gmail.com>
2023-05-31 17:36:56 +0800

    Show the cross lines when pressing on the board Show the cross lines when pressing on the chess board, this make player easy to see where they will place the chess. In this commit, I also try to remove the zombie socket when players leave the room, but it don't work.

--- app/src/main/java/com/example/gobang/peer/ChessBoardASync.java
+++ app/src/main/java/com/example/gobang/peer/ChessBoardASync.java
@@ -28,7 +28,8 @@ public class ChessBoardASync extends View {
     private final Paint circlePaint;
     private float gridSize;
     private final Gobang game;
-    final Paint redPaint;
+    private final Paint redPaint;
+    private final Paint greenPaint;
     public ChessBoardASync(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
@@ -46,6 +47,9 @@ public class ChessBoardASync extends View {
 
         redPaint=new Paint();
         redPaint.setColor(Color.RED);
+
+        greenPaint=new Paint();
+        greenPaint.setColor(Color.GREEN);
 
         background = BitmapFactory.decodeResource(getResources(), R.drawable.chessboard);
     }
@@ -70,7 +74,10 @@ public class ChessBoardASync extends View {
                 ,getWidth(),getHeight(),true);
         canvas.drawBitmap(res,0,0,mBitmapPaint);
         //到這邊
-
+        if(checkX>=0){
+            canvas.drawRect(0, checkY*gridSize+gridSize*0.4f, getWidth(), checkY*gridSize+gridSize*0.6f, greenPaint);
+            canvas.drawRect(checkX*gridSize+gridSize*0.4f, 0, checkX*gridSize+gridSize*0.6f, getHeight(), greenPaint);
+        }
         int[] point = game.getLastMove();
         if(point[0]>=0){
             canvas.drawRect(point[0]*gridSize+gridSize*0.1f, point[1]*gridSize+gridSize*0.1f, point[0]*gridSize+gridSize*0.9f, point[1]*gridSize+gridSize*0.9f, redPaint);
@@ -88,19 +95,40 @@ public class ChessBoardASync extends View {
         this.chessPlaceHandler = chessPlaceHandler;
     }
     /**覆寫:偵測使用者觸碰螢幕的事件*/
+    private int checkX=-1,checkY=-1;
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         byte x = (byte) (event.getX() / gridSize);
         byte y = (byte) (event.getY() / gridSize);
 
         // 確保x, y 不會out of bound
-        if (x == 13){ x = 12;}
-        if (y == 13){ y = 12;}
-        // 確保這個落點是合理的，並且 getTouchPointSync 正在等
-        if (event.getAction() == MotionEvent.ACTION_UP && game.getChessColor(x, y) == 0 && chessPlaceHandler != null) {
-            chessPlaceHandler.run(x, y);
-            chessPlaceHandler = null;
+        if(x>12||y>12||x<0||y<0){
+            checkY=checkX=-1;
+            invalidate();
+            return true;
         }
+        switch (event.getAction()){
+            case MotionEvent.ACTION_UP:
+                if (game.getChessColor(x, y) == 0 && chessPlaceHandler != null) {
+                    chessPlaceHandler.run(x, y);
+                    chessPlaceHandler = null;
+                }
+                checkY=checkX=-1;
+                break;
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                if (game.getChessColor(x, y) == 0) {
+                    checkY=y;
+                    checkX=x;
+                }else{
+                    checkY=checkX=-1;
+                }
+                break;
+            default:
+                checkY=checkX=-1;
+        }
+        // 確保這個落點是合理的，並且 getTouchPointSync 正在等
+
         invalidate();
         return true;
     }
